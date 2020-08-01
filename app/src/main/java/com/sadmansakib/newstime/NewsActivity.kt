b57042a68c36eeb63764a2315fd50d7e13b36e54
sadmansakib <ssadman8@gmail.com>
2020-08-01 20:06:39 +0600

    initial project
    Signed-off-by: sadmansakib <ssadman8@gmail.com>

---
+++ app/src/main/java/com/sadmansakib/newstime/NewsActivity.kt
@@ -0,0 +1,11 @@
+package com.sadmansakib.newstime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+    }
+}
