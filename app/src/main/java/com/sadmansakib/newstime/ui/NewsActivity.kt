f82845cfa17e9f85b683f42599b57d930aae0214
sadmansakib <ssadman8@gmail.com>
2020-08-02 14:45:42 +0600

    add initial navigation system
    Signed-off-by: sadmansakib <ssadman8@gmail.com>

--- app/src/main/java/com/sadmansakib/newstime/ui/NewsActivity.kt
+++ app/src/main/java/com/sadmansakib/newstime/ui/NewsActivity.kt
@@ -2,11 +2,16 @@ package com.sadmansakib.newstime.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.sadmansakib.newstime.R
+import kotlinx.android.synthetic.main.activity_news.*
 
 class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
+
+        bottom_nav_view.setupWithNavController(nav_host_fragment.findNavController())
     }
 }
