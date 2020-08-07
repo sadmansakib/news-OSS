fb0604faf2be5ea255cf61e6de3107c88c9ea7cd
sadmansakib <ssadman8@gmail.com>
2020-08-07 19:23:04 +0600

    bringup dependency injection using hilt
    Signed-off-by: sadmansakib <ssadman8@gmail.com>

--- app/src/main/java/com/sadmansakib/newstime/ui/fragments/AllNewsFragment.kt
+++ app/src/main/java/com/sadmansakib/newstime/ui/fragments/AllNewsFragment.kt
@@ -2,7 +2,9 @@ package com.sadmansakib.newstime.ui.fragments
 
 import androidx.fragment.app.Fragment
 import com.sadmansakib.newstime.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class AllNewsFragment : Fragment(R.layout.fragment_all_news) {
 
 }
--- app/src/main/java/com/sadmansakib/newstime/ui/fragments/TopNewsFragment.kt
+++ app/src/main/java/com/sadmansakib/newstime/ui/fragments/TopNewsFragment.kt
@@ -3,8 +3,10 @@ package com.sadmansakib.newstime.ui.fragments
 
 import androidx.fragment.app.Fragment
 import com.sadmansakib.newstime.R
+import dagger.hilt.android.AndroidEntryPoint
 
 
+@AndroidEntryPoint
 class TopNewsFragment : Fragment(R.layout.fragment_top_news) {
 
 }
