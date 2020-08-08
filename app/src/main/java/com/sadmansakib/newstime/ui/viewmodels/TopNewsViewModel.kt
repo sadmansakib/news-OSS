b050a00051054c422a148fe0eaade10cc986cb60
sadmansakib <ssadman8@gmail.com>
2020-08-08 17:43:14 +0600

    handle network response using flow and network status. This is allowing us to move response handling from viewmodel to repository
    Signed-off-by: sadmansakib <ssadman8@gmail.com>

--- app/src/main/java/com/sadmansakib/newstime/ui/viewmodels/TopNewsViewModel.kt
+++ app/src/main/java/com/sadmansakib/newstime/ui/viewmodels/TopNewsViewModel.kt
@@ -5,22 +5,31 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sadmansakib.newstime.models.NewsResponse
+import com.sadmansakib.newstime.network.DataState
 import com.sadmansakib.newstime.repositories.NewsRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 
 class TopNewsViewModel @ViewModelInject constructor(
     private val newsRepository: NewsRepository
 ): ViewModel() {
 
-    val breakingNews: MutableLiveData<NewsResponse> = MutableLiveData()
+    val breakingNews: MutableLiveData<DataState<NewsResponse>> = MutableLiveData()
     private var breakingNewsPage = 1
 
     init {
         getTopNews(country = "us")
     }
 
-    fun getTopNews(country: String)= viewModelScope.launch {
-        val response = newsRepository.getTopNews(country, category="technology" , page= breakingNewsPage)
-        breakingNews.postValue(response.body())
+    @ExperimentalCoroutinesApi
+    private fun getTopNews(country: String)= viewModelScope.launch {
+        newsRepository.getTopNews(country, category="technology" , page= breakingNewsPage)
+            .onEach {
+                dataState ->  breakingNews.value = dataState
+            }
+            .launchIn(viewModelScope)
     }
 }
