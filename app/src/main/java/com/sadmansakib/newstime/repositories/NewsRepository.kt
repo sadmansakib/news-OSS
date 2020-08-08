6be38a87ebd1493b365f023fe68fa142e837de26
Sadman Sakib <ssadman8@gmail.com>
2020-08-08 18:18:14 +0600

    Dependency injection (#3)
    * bring up API connection
    
    * added network module
    * written dummy repository response
    * network status OK
    Signed-off-by: sadmansakib <ssadman8@gmail.com>
    
    * handle network response using flow and network status. This is allowing us to move response handling from viewmodel to repository
    
    Signed-off-by: sadmansakib <ssadman8@gmail.com>
    
    * Handle api request with flow and state. This allows response check in repository
    
    Signed-off-by: sadmansakib <ssadman8@gmail.com>
    
    * rewritten with flow and state handling. This allows us to manipulate response in repository
    
    Signed-off-by: sadmansakib <ssadman8@gmail.com>

--- app/src/main/java/com/sadmansakib/newstime/repositories/NewsRepository.kt
+++ app/src/main/java/com/sadmansakib/newstime/repositories/NewsRepository.kt
@@ -1,13 +1,34 @@
 package com.sadmansakib.newstime.repositories
 
+import com.sadmansakib.newstime.models.NewsResponse
+import com.sadmansakib.newstime.network.DataState
 import com.sadmansakib.newstime.network.NewsNetworkInterface
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.Response
+import java.lang.Exception
 import javax.inject.Inject
 
 
 class NewsRepository @Inject constructor(
     private val networkInterface: NewsNetworkInterface
 ) {
-    suspend fun getTopNews(countryCode: String,
+
+    suspend fun getTopNews(countryCode: String, 
                            category: String ,
-                           page: Int) = networkInterface.getTopNews(countryCode, category, page)
+                           page: Int): Flow<DataState<NewsResponse>> = flow {
+        emit(DataState.LOADING)
+        try {
+            val response = networkInterface.getTopNews(
+                countryCode,category,page
+            )
+            if (response.isSuccessful){
+                response.body()?.let {newsResponse ->
+                    emit(DataState.SUCCESS(newsResponse))
+                }
+            }
+        }catch (e : Exception){
+            emit(DataState.ERROR(e))
+        }
+    }
 }
